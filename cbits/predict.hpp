fabeed4892fa0be6e8c58a0cd4435ad3106c6919
Abdul Dakkak <abduld@wolfram.com>
2017-11-23 21:53:08 -0600

    remove debug statements
    Former-commit-id: c0065a59d6877bbc7bf701d93f73605288a0742d [formerly f1bb0368bfc6fc8e2f761f90b54b821e0b2c29d6] [formerly 589304ca65185183a6c2a345702afe6bcd2a7b0f [formerly 589304ca65185183a6c2a345702afe6bcd2a7b0f [formerly ee14e93d3af87b095d94620c0c4a5a7e030912a1]]]
    Former-commit-id: d1dbd2123638bae4176055e980ad6e52a74295bc [formerly 006b7656375e4da0c072a728b687e9c76140faa8]
    Former-commit-id: f5fcecae4d74b60487b192b855ab823771e95a4c
    Former-commit-id: 78922326917eb68f6db5e72851c8bba5f8b65b8a

--- cbits/predict.hpp
+++ cbits/predict.hpp
@@ -6,8 +6,8 @@ extern "C" {
 #endif // __cplusplus
 
 typedef void *PredictorContext;
-PredictorContext NewTensorRT(char *model_file, char *trained_file, int batchSize,
-                             char *outputLayer);
+PredictorContext NewTensorRT(char *model_file, char *trained_file,
+                             int batchSize, char *outputLayer);
 void DeleteTensorRT(PredictorContext pred);
 const char *PredictTensorRT(PredictorContext pred, float *input,
                             const char *input_layer_name,
