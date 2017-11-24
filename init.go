720a5ecdef7fb17dfd819bd3f07aa1d5512ae9a0
Abdul Dakkak <abduld@wolfram.com>
2017-11-23 22:04:04 -0600

    add init file
    Former-commit-id: 8dc0dc2abeb1bd13837be9816dea1336ac047205 [formerly f786945049e66f98b55250d5d99f3a18a00fade9] [formerly 431c3038b719cdc606618ffd80bc8aa1cf709190 [formerly 431c3038b719cdc606618ffd80bc8aa1cf709190 [formerly 6d012163193a0f4c91b349d63c157a111b904fed]]]
    Former-commit-id: 42451c9e8232b21eeaf21cd2643e91a2e990e7c1 [formerly 1102b1132c814f8dcf4b0a2644caf834ea60b71b]
    Former-commit-id: 5feec59eb4acb04b0f1a7cec46d95f6848ce5edd
    Former-commit-id: 24183b06830341637d2137842494d92cb549e53d

---
+++ init.go
@@ -0,0 +1,17 @@
+package tensorrt
+
+import (
+	"github.com/rai-project/config"
+	"github.com/rai-project/logger"
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	log *logrus.Entry
+)
+
+func init() {
+	config.AfterInit(func() {
+		log = logger.New().WithField("pkg", "go-tensorrt")
+	})
+}
