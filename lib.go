bb0c96a5c6b1de87d865ba2e5e340de047513d86
Abdul Dakkak <abduld@wolfram.com>
2017-11-30 02:57:36 +0000

    add lookup for /opt/tensorrt
    Former-commit-id: fb29a999b179e57a5bcf903fc941c07bc5c22222 [formerly 5d07a3f2f978a47d3e1b2e87ecabe3a50db62650] [formerly e625eaf3bdba6eabd3cdda38d98a98cfcbddd964 [formerly e625eaf3bdba6eabd3cdda38d98a98cfcbddd964 [formerly cdd8dc79dfbc3e6c4da636d4e875566b7f2b681f]]]
    Former-commit-id: 1ba86785b7ce02a74e4fa1d8b0e0da8f5f9d9a70 [formerly f6439869d1403c9da002078b687c04cae86da1ac]
    Former-commit-id: 303bb27867f2d5cfd6345e4f568382cf9775dc0c
    Former-commit-id: f6f2ba3f7b6e3892d5740d7e7eae5786a1335506

--- lib.go
+++ lib.go
@@ -5,7 +5,7 @@ package tensorrt
 // #cgo LDFLAGS: -lnvinfer -lnvcaffe_parser -lcudart -L${SRCDIR} -lstdc++
 // #cgo CXXFLAGS: -std=c++11  -O3 -Wall -g
 // #cgo CXXFLAGS: -Wno-sign-compare -Wno-unused-function
-// #cgo CXXFLAGS: -I/usr/local/cuda/include -I/usr/include/x86_64-linux-gnu
+// #cgo CXXFLAGS: -I/usr/local/cuda/include -I/usr/include/x86_64-linux-gnu -I/opt/frameworks/tensorrt/include
 // #cgo CXXFLAGS: -I${SRCDIR}/cbits -I${SRCDIR}/cbits/util -I${SRCDIR}/cuda
-// #cgo linux,amd64 LDFLAGS: -L/usr/local/cuda/lib64 -lcudart
+// #cgo linux,amd64 LDFLAGS: -L/usr/local/cuda/lib64 -L/opt/frameworks/tensorrt/lib -lcudart
 import "C"
