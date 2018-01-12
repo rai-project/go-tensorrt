e497f3dbd0c93d39d8ec29aaf17547dde71e36c0
abdul dakkak <abduld@wolfram.com>
2018-01-12 14:38:30 +0000

    Update lib.go
    Former-commit-id: e18e7b658fab08a07aa3b31f27168b88b585a376 [formerly f6cfb9b595410112b5b984e25f8fc215eddc9be0] [formerly bea102332364fbe6bb4abdc590eda0b5dbf898cb [formerly bea102332364fbe6bb4abdc590eda0b5dbf898cb [formerly a931004f2519b7b9d8e542d3a3ec9f61cb41a82f]]]
    Former-commit-id: 7e1da9aaf5819b92add7a6148eb9def915c9d543 [formerly 78660a3b85a7c0728dc7ae747d05631437a85e0d]
    Former-commit-id: 6ea6bc3903f1d4e211368bcae93bdf99ac257b21
    Former-commit-id: f26e6322e3134e1954fb4afb05b382d0e825aeeb

--- lib.go
+++ lib.go
@@ -4,10 +4,9 @@ package tensorrt
 
 // #cgo LDFLAGS: -lnvinfer -lnvcaffe_parser -lcudart -L${SRCDIR} -lstdc++
 // #cgo CXXFLAGS: -std=c++11  -O3 -Wall -g
-// #cgo CXXFLAGS: -Wno-sign-compare -Wno-unused-function
-// #cgo linux,amd64 CXXFLAGS: -I/usr/local/cuda-9.0/include -I/usr/include/x86_64-linux-gnu -I/opt/frameworks/tensorrt/include
-// #cgo linux,arm64 CXXFLAGS: -I/usr/local/cuda-9.0/include -I/usr/include/aarch64-linux-gnu -I/opt/frameworks/tensorrt/include
-// #cgo CXXFLAGS: -I${SRCDIR}/cbits
-// #cgo linux,amd64 LDFLAGS: -L/usr/local/cuda/lib64 -L/opt/frameworks/tensorrt/lib -lcudart
-// #cgo linux,arm64 LDFLAGS: -L/usr/local/cuda-9.0/lib64 -L/opt/frameworks/tensorrt/lib -lcudart
+// #cgo CXXFLAGS: -Wno-sign-compare -Wno-unused-function -I${SRCDIR}/cbits
+// #cgo linux,!ppc64le CXXFLAGS: -I/usr/local/cuda/include -I/opt/frameworks/tensorrt/include
+// #cgo linux,amd64 CXXFLAGS: -I/usr/include/x86_64-linux-gnu 
+// #cgo linux,arm64 CXXFLAGS: -I/usr/include/aarch64-linux-gnu 
+// #cgo linux,!ppc64le  LDFLAGS: -L/usr/local/cuda/lib64 -L/opt/frameworks/tensorrt/lib -lcudart
 import "C"
