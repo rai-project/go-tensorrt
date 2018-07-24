947303b408247fb3aa4aea6568da56dc6b1e1aef
abhiutd <abhibits2012@gmail.com>
2018-07-24 11:19:49 -0500

    binding working

--- lib.go
+++ lib.go
@@ -5,8 +5,8 @@ package tensorrt
 // #cgo LDFLAGS: -lnvinfer -lnvcaffe_parser -lcudart -L${SRCDIR} -lstdc++
 // #cgo CXXFLAGS: -std=c++11  -O3 -Wall -g
 // #cgo CXXFLAGS: -Wno-sign-compare -Wno-unused-function -I${SRCDIR}/cbits
-// #cgo linux,!ppc64le CXXFLAGS: -I/usr/local/cuda/include -I/opt/frameworks/tensorrt/include
+// #cgo linux,!ppc64le CXXFLAGS: -I/usr/local/cuda/include -I/home/as29/my_tensorrt/TensorRT-4.0.1.6/include
 // #cgo linux,amd64 CXXFLAGS: -I/usr/include/x86_64-linux-gnu 
 // #cgo linux,arm64 CXXFLAGS: -I/usr/include/aarch64-linux-gnu 
-// #cgo linux,!ppc64le  LDFLAGS: -L/usr/local/cuda/lib64 -L/opt/frameworks/tensorrt/lib -lcudart
+// #cgo linux,!ppc64le  LDFLAGS: -L/usr/local/cuda/lib64 -L/home/as29/my_tensorrt/TensorRT-4.0.1.6/lib -L/usr/lib/x86_64-linux-gnu -lcudnn -lcudart
 import "C"
