c5fdd2265559a5e436a36dd1553206b33ea9f35c
Ubuntu <ubuntu@ip-172-31-55-250.ec2.internal>
2018-09-27 20:20:34 +0000

    update lib.go
    Former-commit-id: bacae0401037807cc3165a8acc096a856eeadafb [formerly bbb8db30b1f61651c43c428a3bf7360e71e14f1e] [formerly 49858350bb24d89d36dffe96b7cdd51fa0dd4271 [formerly 49858350bb24d89d36dffe96b7cdd51fa0dd4271 [formerly 38f618b5b605b21550c2feded31a1a245134bdd8]]]
    Former-commit-id: 2d9056e6b95a2f49d908dc9581daca80523b682b [formerly 9b33186d8fed6efffdd61d29f9a69b2d6b42cb5b]
    Former-commit-id: fe6b45b074fee135f8bcec141c0317c34b4444c1
    Former-commit-id: 06f170e9fe876da290578785aa4e6aaa0dbc8117

--- lib.go
+++ lib.go
@@ -6,7 +6,7 @@ package tensorrt
 // #cgo CXXFLAGS: -std=c++11  -O3 -Wall -g
 // #cgo CXXFLAGS: -Wno-sign-compare -Wno-unused-function -I${SRCDIR}/cbits
 // #cgo linux,!ppc64le CXXFLAGS: -I/usr/local/cuda/include
-// #cgo linux,amd64 CXXFLAGS: -I/usr/include/x86_64-linux-gnu
+// #cgo linux,amd64 CXXFLAGS: -I/opt/tensorrt/include -I/usr/include/x86_64-linux-gnu
 // #cgo linux,arm64 CXXFLAGS: -I/usr/include/aarch64-linux-gnu
-// #cgo linux,!ppc64le  LDFLAGS: -L/usr/local/cuda/lib64 -L/usr/lib/x86_64-linux-gnu -lcudnn -lcudart
+// #cgo linux,!ppc64le  LDFLAGS: -L/usr/local/cuda/lib64 -L/opt/tensorrt/lib -L/usr/lib/x86_64-linux-gnu -lcudnn -lcudart
 import "C"
