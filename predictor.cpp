db7b2cd7b1b78de14eb1a222a0184d6394a79048
Cheng Li <cli99@illinois.edu>
2018-11-07 11:29:34 -0600

    work

--- predictor.cpp
+++ predictor.cpp
@@ -18,27 +18,34 @@ using namespace nvcaffeparser1;
 
 using json = nlohmann::json;
 
-class Logger : public ILogger {
-  void log(Severity severity, const char *msg) override {
+class Logger : public ILogger
+{
+  void log(Severity severity, const char *msg) override
+  {
     // suppress info-level messages
-    if (severity != Severity::kINFO) {
+    if (severity != Severity::kINFO)
+    {
       std::cout << msg << std::endl;
     }
   }
 } gLogger;
 
-#define CHECK(status)                                                          \
-  {                                                                            \
-    if (status != 0) {                                                         \
-      std::cerr << "Cuda failure on line " << __LINE__                         \
-                << " status =  " << status << "\n";                            \
-    }                                                                          \
+#define CHECK(status)                                  \
+  {                                                    \
+    if (status != 0)                                   \
+    {                                                  \
+      std::cerr << "Cuda failure on line " << __LINE__ \
+                << " status =  " << status << "\n";    \
+    }                                                  \
   }
 
-class Profiler : public IProfiler {
+class Profiler : public IProfiler
+{
 public:
-  Profiler(profile *prof) : prof_(prof) {
-    if (prof_ == nullptr) {
+  Profiler(profile *prof) : prof_(prof)
+  {
+    if (prof_ == nullptr)
+    {
       return;
     }
     prof_->start(); // reset start time
@@ -51,9 +58,11 @@ public:
    * definition
    * \param ms the time in milliseconds to execute the layer
    */
-  virtual void reportLayerTime(const char *layer_name, float ms) {
+  virtual void reportLayerTime(const char *layer_name, float ms)
+  {
 
-    if (prof_ == nullptr) {
+    if (prof_ == nullptr)
+    {
       return;
     }
 
@@ -78,25 +87,29 @@ private:
   timestamp_t current_time_{};
 };
 
-class Predictor {
+class Predictor
+{
 public:
   Predictor(ICudaEngine *engine, IExecutionContext *context, int batch_size,
             std::string input_layer_name, std::string output_layer_name, int shape_len)
       : engine_(engine), context_(context), batch_(batch_size),
         input_layer_name_(input_layer_name),
-        output_layer_name_(output_layer_name), shape_len_(shape_len){   
-};
+        output_layer_name_(output_layer_name), shape_len_(shape_len){};
 
   void Predict(float *imageData);
 
-  ~Predictor() {
-    if (context_) {
+  ~Predictor()
+  {
+    if (context_)
+    {
       context_->destroy();
     }
-    if (engine_) {
+    if (engine_)
+    {
       engine_->destroy();
     }
-    if (prof_) {
+    if (prof_)
+    {
       prof_->reset();
       delete prof_;
       prof_ = nullptr;
@@ -115,11 +128,14 @@ public:
   bool profile_enabled_{false};
 };
 
-void Predictor::Predict(float *inputData) {
-  if (engine_->getNbBindings() != 2) {
+void Predictor::Predict(float *inputData)
+{
+  if (engine_->getNbBindings() != 2)
+  {
     std::cerr << "tensorrt prediction error on " << __LINE__ << "\n";
   }
-  if (context_ == nullptr) {
+  if (context_ == nullptr)
+  {
     std::cerr << "tensorrt prediction error on " << __LINE__
               << " :: null context_\n";
   }
@@ -145,8 +161,8 @@ void Predictor::Predict(float *inputData) {
   CHECK(cudaMalloc((void **)&input_layer, input_byte_size));
   CHECK(cudaMalloc((void **)&output_layer, output_byte_size));
 
-  std::cerr << "size of input = " <<  input_byte_size << "\n";
-  std::cerr << "size of output = " << output_byte_size << "\n";
+  // std::cerr << "size of input = " <<  input_byte_size << "\n";
+  // std::cerr << "size of output = " << output_byte_size << "\n";
 
   // DMA the input to the GPU,  execute the batch_size  asynchronously, and DMA
   // it back:
@@ -173,8 +189,10 @@ void Predictor::Predict(float *inputData) {
 
 PredictorContext NewTensorRT(char *deploy_file, char *weights_file,
                              int batch_size, char *input_layer_name,
-                             char *output_layer_name, int shape_len) {
-  try {
+                             char *output_layer_name, int shape_len)
+{
+  try
+  {
     IBuilder *builder = createInferBuilder(gLogger);
     INetworkDefinition *network = builder->createNetwork();
     ICaffeParser *parser = createCaffeParser();
@@ -183,7 +201,8 @@ PredictorContext NewTensorRT(char *deploy_file, char *weights_file,
         parser->parse(deploy_file, weights_file, *network, DataType::kFLOAT);
 
     auto loc = blobNameToTensor->find(output_layer_name);
-    if (loc == nullptr) {
+    if (loc == nullptr)
+    {
       std::cerr << "cannot find " << output_layer_name
                 << " in blobNameToTensor\n";
       return nullptr;
@@ -198,37 +217,46 @@ PredictorContext NewTensorRT(char *deploy_file, char *weights_file,
         new Predictor(engine, context, batch_size, input_layer_name,
                       output_layer_name, shape_len);
     return (PredictorContext)pred;
-  } catch (const std::invalid_argument &ex) {
+  }
+  catch (const std::invalid_argument &ex)
+  {
     return nullptr;
   }
 }
 
 void InitTensorRT() {}
 
-void PredictTensorRT(PredictorContext pred, float *inputData) {
+void PredictTensorRT(PredictorContext pred, float *inputData)
+{
   auto predictor = (Predictor *)pred;
-  if (predictor == nullptr) {
+  if (predictor == nullptr)
+  {
     return;
   }
   predictor->Predict(inputData);
   return;
 }
 
-const float *GetPredictionsTensorRT(PredictorContext pred) {
+const float *GetPredictionsTensorRT(PredictorContext pred)
+{
   auto predictor = (Predictor *)pred;
-  if (predictor == nullptr) {
+  if (predictor == nullptr)
+  {
     return nullptr;
   }
   return predictor->result_;
 }
 
-void DeleteTensorRT(PredictorContext pred) {
+void DeleteTensorRT(PredictorContext pred)
+{
   auto predictor = (Predictor *)pred;
-  if (predictor == nullptr) {
+  if (predictor == nullptr)
+  {
     return;
   }
 
-  if (predictor->result_) {
+  if (predictor->result_)
+  {
     delete[] predictor->result_;
     predictor->result_ = nullptr;
   }
@@ -236,50 +264,66 @@ void DeleteTensorRT(PredictorContext pred) {
 }
 
 void StartProfilingTensorRT(PredictorContext pred, const char *name,
-                            const char *metadata) {
+                            const char *metadata)
+{
   auto predictor = (Predictor *)pred;
-  if (predictor == nullptr) {
+  if (predictor == nullptr)
+  {
     return;
   }
-  if (name == nullptr) {
+  if (name == nullptr)
+  {
     name = "";
   }
-  if (metadata == nullptr) {
+  if (metadata == nullptr)
+  {
     metadata = "";
   }
-  if (predictor->prof_ == nullptr) {
+  if (predictor->prof_ == nullptr)
+  {
     predictor->prof_ = new profile(name, metadata);
-  } else {
+  }
+  else
+  {
     predictor->prof_->reset();
   }
 }
 
-void EndProfilingTensorRT(PredictorContext pred) {
+void EndProfilingTensorRT(PredictorContext pred)
+{
   auto predictor = (Predictor *)pred;
-  if (predictor == nullptr) {
+  if (predictor == nullptr)
+  {
     return;
   }
-  if (predictor->prof_) {
+  if (predictor->prof_)
+  {
     predictor->prof_->end();
   }
 }
 
-void DisableProfilingTensorRT(PredictorContext pred) {
+void DisableProfilingTensorRT(PredictorContext pred)
+{
   auto predictor = (Predictor *)pred;
-  if (predictor == nullptr) {
+  if (predictor == nullptr)
+  {
     return;
   }
-  if (predictor->prof_) {
+  if (predictor->prof_)
+  {
     predictor->prof_->reset();
   }
 }
 
-char *ReadProfileTensorRT(PredictorContext pred) {
+char *ReadProfileTensorRT(PredictorContext pred)
+{
   auto predictor = (Predictor *)pred;
-  if (predictor == nullptr) {
+  if (predictor == nullptr)
+  {
     return strdup("");
   }
-  if (predictor->prof_ == nullptr) {
+  if (predictor->prof_ == nullptr)
+  {
     return strdup("");
   }
   const auto s = predictor->prof_->read();
@@ -287,17 +331,21 @@ char *ReadProfileTensorRT(PredictorContext pred) {
   return strdup(cstr);
 }
 
-int GetShapeLenTensorRT(PredictorContext pred) {
+int GetShapeLenTensorRT(PredictorContext pred)
+{
   auto predictor = (Predictor *)pred;
-  if (predictor == nullptr) {
+  if (predictor == nullptr)
+  {
     return 0;
   }
   return predictor->shape_len_;
 }
 
-int GetPredLenTensorRT(PredictorContext pred) {
+int GetPredLenTensorRT(PredictorContext pred)
+{
   auto predictor = (Predictor *)pred;
-  if (predictor == nullptr) {
+  if (predictor == nullptr)
+  {
     return 0;
   }
   return predictor->pred_len_;
