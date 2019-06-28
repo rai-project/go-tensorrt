03b873cd625942b960c6ea1f78b12a10e399270c
Abdul Dakkak <dakkak@illinois.edu>
2019-06-28 15:10:21 +0000

    work

--- examples/batch_mlmodelscope/main.go
+++ examples/batch_mlmodelscope/main.go
@@ -12,7 +12,6 @@ import (
 	"github.com/anthonynsimon/bild/imgio"
 	"github.com/anthonynsimon/bild/transform"
 	"github.com/k0kubun/pp"
-
 	"github.com/rai-project/config"
 	"github.com/rai-project/dlframework"
 	"github.com/rai-project/dlframework/framework/feature"
@@ -27,73 +26,78 @@ import (
 )
 
 var (
-	batchSize   = 64
+	batchSize   = 1
+	model       = "bvlc_alexnet"
+	shape       = []int{1, 3, 227, 227}
+	mean        = []float32{123, 117, 104}
+	scale       = []float32{1.0, 1.0, 1.0}
+	imgDir, _   = filepath.Abs("../../_fixtures")
+	imgPath     = filepath.Join(imgDir, "platypus.jpg")
 	graph_url   = "https://raw.githubusercontent.com/BVLC/caffe/master/models/bvlc_alexnet/deploy.prototxt"
 	weights_url = "http://dl.caffe.berkeleyvision.org/bvlc_alexnet.caffemodel"
-	synset_url  = "http://data.dmlc.ml/mxnet/models/imagenet/synset.txt"
+	synset_url  = "http://s3.amazonaws.com/store.carml.org/synsets/imagenet/synset.txt"
 )
 
 // convert go Image to 1-dim array
-func cvtImageTo1DArray(src image.Image, mean []float32) ([]float32, error) {
+func cvtRGBImageToNCHW1DArray(src image.Image, mean []float32, scale []float32) ([]float32, error) {
 	if src == nil {
 		return nil, fmt.Errorf("src image nil")
 	}
 
-	b := src.Bounds()
-	h := b.Max.Y - b.Min.Y // image height
-	w := b.Max.X - b.Min.X // image width
+	in := src.Bounds()
+	height := in.Max.Y - in.Min.Y // image height
+	width := in.Max.X - in.Min.X  // image width
 
-	res := make([]float32, 3*h*w)
-	for y := 0; y < h; y++ {
-		for x := 0; x < w; x++ {
-			r, g, b, _ := src.At(x+b.Min.X, y+b.Min.Y).RGBA()
-			res[y*w+x] = float32(b>>8) - mean[0]
-			res[w*h+y*w+x] = float32(g>>8) - mean[1]
-			res[2*w*h+y*w+x] = float32(r>>8) - mean[2]
+	out := make([]float32, 3*height*width)
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			r, g, b, _ := src.At(x+in.Min.X, y+in.Min.Y).RGBA()
+			out[y*width+x] = (float32(b) - mean[2]) / scale[2]
+			out[width*height+y*width+x] = (float32(g) - mean[1]) / scale[1]
+			out[2*width*height+y*width+x] = (float32(r) - mean[0]) / scale[0]
 		}
 	}
 
-	return res, nil
+	return out, nil
 }
 
 func main() {
 	defer tracer.Close()
 
 	dir, _ := filepath.Abs("../tmp")
+	dir = filepath.Join(dir, model)
 	graph := filepath.Join(dir, "deploy.prototxt")
-	weights := filepath.Join(dir, "bvlc_alexnet.caffemodel")
+	weights := filepath.Join(dir, model+".caffemodel")
 	synset := filepath.Join(dir, "synset.txt")
 
-	if _, err := os.Stat(graph); os.IsNotExist(err) {
+	if !com.IsFile(graph) {
 		if _, err := downloadmanager.DownloadInto(graph_url, dir); err != nil {
 			panic(err)
 		}
 	}
-	if _, err := os.Stat(weights); os.IsNotExist(err) {
-
+	if !com.IsFile(weights) {
 		if _, err := downloadmanager.DownloadInto(weights_url, dir); err != nil {
 			panic(err)
 		}
 	}
-	if _, err := os.Stat(synset); os.IsNotExist(err) {
-
+	if !com.IsFile(synset) {
 		if _, err := downloadmanager.DownloadInto(synset_url, dir); err != nil {
 			panic(err)
 		}
 	}
 
-	imgDir, _ := filepath.Abs("../_fixtures")
-	imagePath := filepath.Join(imgDir, "platypus.jpg")
-
-	img, err := imgio.Open(imagePath)
+	img, err := imgio.Open(imgPath)
 	if err != nil {
 		panic(err)
 	}
 
+	height := shape[2]
+	width := shape[3]
+
 	var input []float32
 	for ii := 0; ii < batchSize; ii++ {
-		resized := transform.Resize(img, 227, 227, transform.Linear)
-		res, err := cvtImageTo1DArray(resized, []float32{123, 117, 104})
+		resized := transform.Resize(img, height, width, transform.Linear)
+		res, err := cvtRGBImageToNCHW1DArray(resized, mean, scale)
 		if err != nil {
 			panic(err)
 		}
@@ -112,18 +116,28 @@ func main() {
 	span, ctx := tracer.StartSpanFromContext(ctx, tracer.FULL_TRACE, "tensorrt_batch")
 	defer span.Finish()
 
+	in := options.Node{
+		Key:   "data",
+		Shape: shape,
+	}
+
+	
 	predictor, err := tensorrt.New(
 		ctx,
 		options.WithOptions(opts),
 		options.Device(device, 0),
 		options.Graph([]byte(graph)),
 		options.Weights([]byte(weights)),
-		options.InputNode("data", []int{3, 227, 227}),
-		options.OutputNode("prob"),
 		options.BatchSize(batchSize),
+		options.InputNodes([]options.Node{in}),
+		options.OutputNodes([]options.Node{
+			options.Node{
+				Dtype: gotensor.Float32,
+			},
+		}),
 	)
 	if err != nil {
-		panic(err)
+		panic(fmt.Sprintf("%v", err))
 	}
 	defer predictor.Close()
 
@@ -132,10 +146,13 @@ func main() {
 		panic(err)
 	}
 
+	enableCupti := false
 	var cu *cupti.CUPTI
-	cu, err = cupti.New(cupti.Context(ctx))
-	if err != nil {
-		panic(err)
+	if enableCupti {
+		cu, err = cupti.New(cupti.Context(ctx))
+		if err != nil {
+			panic(err)
+		}
 	}
 
 	predictor.StartProfiling("predict", "")
@@ -147,8 +164,11 @@ func main() {
 
 	predictor.EndProfiling()
 
-	cu.Wait()
-	cu.Close()
+
+	if enableCupti {
+		cu.Wait()
+		cu.Close()
+	}
 
 	profBuffer, err := predictor.ReadProfile()
 	if err != nil {
@@ -162,7 +182,7 @@ func main() {
 	}
 	t.Publish(ctx, tracer.FRAMEWORK_TRACE)
 
-	output, err := predictor.ReadPredictionOutput(ctx)
+	output, err := predictor.ReadOutputData(ctx, 0)
 	if err != nil {
 		panic(err)
 	}
